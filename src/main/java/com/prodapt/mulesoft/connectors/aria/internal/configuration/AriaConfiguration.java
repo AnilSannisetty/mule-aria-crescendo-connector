4f2c6e578391e5a574c53bf733be41cf1341b3bc
Abhijith Vijayan <abhijith.v@prodapt.com>
2021-09-15 15:46:47 +0530

    Created 10 new Aria Operations and modified Aria Configuration class for Aria Version 1.1
    Created 10 new Aria Operations for Aria v1.1:
    update account credentials, update account multiple plans, update account status, update contact, update payment method, update order, update account invoice, get invoice cm details, get statement for invoice and get order.
    Also updated the Aria Configuration classes with the new operations.

--- src/main/java/com/prodapt/mulesoft/connectors/aria/internal/configuration/AriaConfiguration.java
+++ src/main/java/com/prodapt/mulesoft/connectors/aria/internal/configuration/AriaConfiguration.java
@@ -10,12 +10,23 @@ import com.prodapt.mulesoft.connectors.aria.internal.operations.BillingAccountRe
 import com.prodapt.mulesoft.connectors.aria.internal.operations.CreateAcctBillingGroup;
 import com.prodapt.mulesoft.connectors.aria.internal.operations.CreateAcctDunningGroup;
 import com.prodapt.mulesoft.connectors.aria.internal.operations.GetAllAccountDetails;
+import com.prodapt.mulesoft.connectors.aria.internal.operations.GetClientPlanBasic;
+import com.prodapt.mulesoft.connectors.aria.internal.operations.GetInvoiceCMDetails;
 import com.prodapt.mulesoft.connectors.aria.internal.operations.GetInvoiceDetails;
 import com.prodapt.mulesoft.connectors.aria.internal.operations.GetInvoiceHistory;
+import com.prodapt.mulesoft.connectors.aria.internal.operations.GetOrder;
+import com.prodapt.mulesoft.connectors.aria.internal.operations.GetStatementForInvoice;
 import com.prodapt.mulesoft.connectors.aria.internal.operations.ModifyAcctBillingGroup;
+import com.prodapt.mulesoft.connectors.aria.internal.operations.ModifyAcctCredentials;
 import com.prodapt.mulesoft.connectors.aria.internal.operations.ModifyAcctDunningGroup;
+import com.prodapt.mulesoft.connectors.aria.internal.operations.ModifyAcctInvoice;
+import com.prodapt.mulesoft.connectors.aria.internal.operations.ModifyAcctMultiPlans;
+import com.prodapt.mulesoft.connectors.aria.internal.operations.ModifyAcctStatus;
 import com.prodapt.mulesoft.connectors.aria.internal.operations.ModifyCompleteBillingAccount;
+import com.prodapt.mulesoft.connectors.aria.internal.operations.ModifyContact;
+import com.prodapt.mulesoft.connectors.aria.internal.operations.ModifyOrder;
+import com.prodapt.mulesoft.connectors.aria.internal.operations.ModifyPaymentMethod;
 
-@Operations({BillingAccountRegistration.class, ModifyCompleteBillingAccount.class, AssignAccountPlan.class, CreateAcctBillingGroup.class, CreateAcctDunningGroup.class, GetAllAccountDetails.class, GetInvoiceDetails.class, GetInvoiceHistory.class, ModifyAcctBillingGroup.class, ModifyAcctDunningGroup.class})
+@Operations({BillingAccountRegistration.class, ModifyCompleteBillingAccount.class, AssignAccountPlan.class, CreateAcctBillingGroup.class, CreateAcctDunningGroup.class, GetAllAccountDetails.class, GetInvoiceDetails.class, GetInvoiceHistory.class, ModifyAcctBillingGroup.class, ModifyAcctDunningGroup.class, ModifyAcctCredentials.class, ModifyAcctMultiPlans.class, ModifyAcctStatus.class, ModifyContact.class, ModifyPaymentMethod.class, ModifyAcctInvoice.class, GetInvoiceCMDetails.class, GetStatementForInvoice.class, GetOrder.class, ModifyOrder.class})
 @ConnectionProviders(AriaConnectionProvider.class)
 public class AriaConfiguration extends RestConfiguration {}
