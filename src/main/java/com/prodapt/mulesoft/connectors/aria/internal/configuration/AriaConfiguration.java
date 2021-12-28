57f0ffa71f75b0ae101d4090917d9490a9743a3a
Hariharan. M <hariharan.m@prodapt.com>
2021-12-28 13:57:03 +0530

    Change in Aria Configuration file
    Change in Aria Configuration file for version 0.0.1 of Aria Crescendo Connector

--- src/main/java/com/prodapt/mulesoft/connectors/aria/internal/configuration/AriaConfiguration.java
+++ src/main/java/com/prodapt/mulesoft/connectors/aria/internal/configuration/AriaConfiguration.java
@@ -9,13 +9,18 @@ import com.prodapt.mulesoft.connectors.aria.internal.operations.AssignAccountPla
 import com.prodapt.mulesoft.connectors.aria.internal.operations.BillingAccountRegistration;
 import com.prodapt.mulesoft.connectors.aria.internal.operations.CreateAcctBillingGroup;
 import com.prodapt.mulesoft.connectors.aria.internal.operations.CreateAcctDunningGroup;
-import com.prodapt.mulesoft.connectors.aria.internal.operations.GetAllAccountDetails;
+import com.prodapt.mulesoft.connectors.aria.internal.operations.GetAcctDetailsAll;
 import com.prodapt.mulesoft.connectors.aria.internal.operations.GetInvoiceDetails;
 import com.prodapt.mulesoft.connectors.aria.internal.operations.GetInvoiceHistory;
 import com.prodapt.mulesoft.connectors.aria.internal.operations.ModifyAcctBillingGroup;
 import com.prodapt.mulesoft.connectors.aria.internal.operations.ModifyAcctDunningGroup;
 import com.prodapt.mulesoft.connectors.aria.internal.operations.ModifyCompleteBillingAccount;
 
-@Operations({BillingAccountRegistration.class, ModifyCompleteBillingAccount.class, AssignAccountPlan.class, CreateAcctBillingGroup.class, CreateAcctDunningGroup.class, GetAllAccountDetails.class, GetInvoiceDetails.class, GetInvoiceHistory.class, ModifyAcctBillingGroup.class, ModifyAcctDunningGroup.class})
+@Operations({
+	BillingAccountRegistration.class, ModifyCompleteBillingAccount.class,
+	AssignAccountPlan.class, CreateAcctBillingGroup.class, 
+	CreateAcctDunningGroup.class, GetAcctDetailsAll.class, 
+	GetInvoiceDetails.class, GetInvoiceHistory.class, 
+	ModifyAcctBillingGroup.class, ModifyAcctDunningGroup.class})
 @ConnectionProviders(AriaConnectionProvider.class)
 public class AriaConfiguration extends RestConfiguration {}
